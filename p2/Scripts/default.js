2fd866078e2c65a7ff471bed492760d09643a2c7
Kristin Rievley <krievley@gmail.com>
2014-09-29 17:54:13 -0400

    Initial commit

---
+++ p2/Scripts/default.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    //Hide div initially.
+    $("#symNum").css("display", "none");
+    $('h2').css("display", "none");
+
+    $("#sym").click(function () {
+
+        if ($("#sym").is(":checked")) {
+            //show the hidden div
+            $("#symNum").show();
+        }
+        else {
+            //otherwise, hide it
+            $("#symNum").hide();
+        }
+    });
+});
+
