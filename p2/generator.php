74deab239d7576754b5ed6614d60076d43d268b6
Kristin Rievley <krievley@gmail.com>
2014-09-30 13:59:12 -0400

    Fixed duplicate word error.

--- p2/generator.php
+++ p2/generator.php
@@ -8,7 +8,7 @@
 	//$letter = $_POST['letter'];
 	
 	//Scrape word list from paulnoll.com
-	$data = file_get_contents('http://www.paulnoll.com/Books/Clear-English/words-01-02-hundred.html');
+	$data = file_get_contents('http://www.paulnoll.com/Books/Clear-English/words-03-04-hundred.html');
 	$regex = '/<li>([^`]*?)<\/li>/';
 	preg_match_all($regex,$data,$matches);
 
@@ -18,27 +18,23 @@
 	//Loop for number of words entered.
 	for($x=0; $x<$words; $x++) {
 		//Get a random number
-		$random = rand(0, 187);
+		$random = rand(0, 159);
 		//Check to see if array is empty
 		if(sizeof($wordList) == 0) {
 			//if it is, add the current word to the array
 			$wordList[$x] = $matches[1][$random];
-		}
-		//if array is not empty
-		else {
+		} else {
 			//check to see if the word has already been selected before adding it to the array.
 			foreach($wordList as $key){
 				//if the word has been used
-				if( $matches[1][$random] == $wordList[$key]){
+				if($matches[1][$random] == $key){
 					//reduce the word count to try again
 					$x--;
-				}
-				//if the word has NOT been used
-				else {
-					//add the word to the array
+				} else {
+					//add the word to the word list
 					$wordList[$x] = $matches[1][$random];
 				}
+			}
 		}
 	}
-}
-//print_r($wordList);
+print_r($wordList);
